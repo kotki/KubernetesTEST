495fdb75aef5496ad64b05c710666a7502b6bf46
okotkevych <okotkevych@lohika.com>
2019-04-12 12:48:19 +0300

    commit

--- KubernetesTEST/KubernetesTEST/Controllers/ValuesController.cs
+++ KubernetesTEST/KubernetesTEST/Controllers/ValuesController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace KubernetesTEST.Controllers
 {
@@ -10,10 +13,12 @@ namespace KubernetesTEST.Controllers
     public class ValuesController : ControllerBase
     {
         private readonly IConfiguration _config;
+        private readonly IHttpClientFactory _clientFactory;
 
-        public ValuesController(IConfiguration config)
+        public ValuesController(IConfiguration config, IHttpClientFactory clientFactory)
         {
             _config = config;
+            _clientFactory = clientFactory;
         }
 
         [HttpGet]
@@ -34,6 +39,71 @@ namespace KubernetesTEST.Controllers
         public ActionResult<string> GetEnv(string key)
         {
             var result = _config.GetValue<string>(key);
+            return result;
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<string>> GetResult(string address)
+        {
+            //string result = null;
+
+            //if (string.IsNullOrWhiteSpace(address))
+            //{
+            //    result = "Nowhere to go!";
+            //}
+            //else
+            //{
+            //    using (var client = _clientFactory.CreateClient())
+            //    {
+            //        using(var request = new HttpRequestMessage(HttpMethod.Get, address))
+            //        {
+            //            using(var response = await client.SendAsync(request))
+            //            {
+            //                if (response.IsSuccessStatusCode)
+            //                {
+            //                    result = await response.Content.ReadAsStringAsync();
+            //                }
+            //                else
+            //                {
+            //                    result = "Something went wrong with response!";
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+            //return result;
+
+            string result = null;
+
+            try
+            {
+                var client = _clientFactory.CreateClient();
+
+                if (string.IsNullOrWhiteSpace(address))
+                {
+                    result = "Nowhere to go!";
+                }
+                else
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, address);
+                    var response = await client.SendAsync(request);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        result = await response.Content.ReadAsStringAsync();
+                    }
+                    else
+                    {
+                        result = "Something went wrong with response!";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+            }
+
             return result;
         }
     }
--- KubernetesTEST/KubernetesTEST2/Startup.cs
+++ KubernetesTEST/KubernetesTEST2/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace KubernetesTEST
+namespace KubernetesTEST2
 {
     public class Startup
     {
